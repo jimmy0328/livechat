52d12973f44739db07d621df9b6fc8c2a12ba676
jimmy <jimmy@jimmydeMacBook-Pro.local>
2016-07-10 23:45:46 +0800

    add current_user in action cable connection

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
+  mount ActionCable.server => '/cable'
+
   devise_for :users
-   root to: 'rooms#show'
-   mount ActionCable.server => '/cable'
+  root to: 'rooms#show'
 end
